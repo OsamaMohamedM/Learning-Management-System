2717aa6555bb17707b8d781ed551680413d5d2e8
OsamaMohamedM <osamamoahmed242@gmail.com>
2024-12-04 22:29:53 +0200

    Project Structure

---
+++ LMS-Services/LMS-Services/src/main/java/com/fcaiSWE2/LMS_Services/LmsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.fcaiSWE2.LMS_Services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsServicesApplication.class, args);
+	}
+
+}
---
+++ LMS-Services/LMS-Services/src/test/java/com/fcaiSWE2/LMS_Services/LmsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcaiSWE2.LMS_Services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
