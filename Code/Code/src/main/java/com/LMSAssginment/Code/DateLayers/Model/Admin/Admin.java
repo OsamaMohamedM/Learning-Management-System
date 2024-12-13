8c4274fecb182d996c68193006190e8d3114325b
Osama Mohamed <osamamoahmed242@gmail.com>
2024-12-13 23:30:24 +0200

    Update Admin.java

--- Code/Code/src/main/java/com/LMSAssginment/Code/DateLayers/Model/Admin/Admin.java
+++ Code/Code/src/main/java/com/LMSAssginment/Code/DateLayers/Model/Admin/Admin.java
@@ -2,6 +2,7 @@ package com.LMSAssginment.Code.DateLayers.Model.Admin;
 
 import com.LMSAssginment.Code.DateLayers.Model.User;
 import jakarta.persistence.Entity;
+import java.util.Date;
 
 @Entity
 public class Admin extends User {
