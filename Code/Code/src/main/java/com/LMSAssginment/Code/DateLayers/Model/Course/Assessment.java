099bb0ea648ad6acabdcdb7e2091468a90855fba
Osama Mohamed <osamamoahmed242@gmail.com>
2024-12-15 13:00:32 +0200

    Update Assessment.java

--- Code/Code/src/main/java/com/LMSAssginment/Code/DateLayers/Model/Course/Assessment.java
+++ Code/Code/src/main/java/com/LMSAssginment/Code/DateLayers/Model/Course/Assessment.java
@@ -2,15 +2,13 @@ package com.LMSAssginment.Code.DateLayers.Model.Course;
 
 import com.LMSAssginment.Code.DateLayers.Model.Questions.Question;
 import jakarta.persistence.*;
-
 import java.util.Date;
 import java.util.List;
 
 @Entity
 public class Assessment {
     @Id
-    private int id;
-    @Column()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int totalGrades;
     private Double duration;
     private Date startDate;
@@ -20,30 +18,26 @@ public class Assessment {
     @JoinColumn(name = "course_id")
     private Course course;
 
-    @OneToMany(mappedBy = "assessment")
-    private List<Question> questionList;
+    @ManyToMany
+    @JoinTable(
+            name = "assessment_question",
+            joinColumns = @JoinColumn(name = "assessment_id"),
+            inverseJoinColumns = @JoinColumn(name = "question_id")
+    )
+    private List<Question> questions;
 
-    public Assessment(int id, int totalGrades, Double duration, Date startDate, String type, Course course, List<Question> questionList) {
+    public Assessment() {}
+
+    public Assessment( int totalGrades, Double duration, Date startDate, String type, Course course, List<Question> questions) {
         this.id = id;
         this.totalGrades = totalGrades;
         this.duration = duration;
         this.startDate = startDate;
         this.type = type;
         this.course = course;
-        this.questionList = questionList;
+        this.questions = questions;
     }
 
-    public Assessment() {
-    }
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public int getTotalGrades() {
         return totalGrades;
@@ -85,11 +79,16 @@ public class Assessment {
         this.course = course;
     }
 
-    public List<Question> getQuestionList() {
-        return questionList;
+    public List<Question> getQuestions() {
+        return questions;
     }
 
-    public void setQuestionList(List<Question> questionList) {
-        this.questionList = questionList;
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    public  int getNumberOfQuestions()
+    {
+       return  questions.size();
     }
 }
